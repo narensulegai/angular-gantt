eee7fe5024aafa83d2f7c30ffdc03c8e85c22a0b
Naren Sulegai <86naren@gmail.com>
2014-07-29 22:13:43 +0530

    Updating lib

--- javascripts/lib/angular_gantt_chart.js
+++ javascripts/lib/angular_gantt_chart.js
@@ -1,4 +1,4 @@
-angular.module('angularGanttChart', ['views/gantt_action_container.html', 'views/gantt_bar.html', 'views/gantt_bar_container.html', 'views/gantt_chart.html', 'views/gantt_chart_marker.html', 'views/gantt_dragable_bar.html', 'views/gantt_label_container.html', 'views/gantt_resizable_bar.html', 'views/gantt_row.html', 'views/main.html']);
+angular.module('angularGanttChart', ['views/gantt_action_container.html', 'views/gantt_bar.html', 'views/gantt_bar_container.html', 'views/gantt_chart.html', 'views/gantt_chart_marker.html', 'views/gantt_dragable_bar.html', 'views/gantt_label_container.html', 'views/gantt_resizable_bar.html', 'views/gantt_row.html']);
 
 angular.module("views/gantt_action_container.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("views/gantt_action_container.html",
@@ -8,9 +8,9 @@ angular.module("views/gantt_action_container.html", []).run(["$templateCache", f
 angular.module("views/gantt_bar.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("views/gantt_bar.html",
     "<div ng-style=\"{width:width + 'px', left:marginLeft + 'px', top:topOffset + 'px'}\" style=\"position: absolute\"\n" +
-      "     ng-class=\"{'gantt-bar-overlapped':isOverlapped}\">\n" +
-      "  <div ng-transclude></div>\n" +
-      "</div>");
+    "     ng-class=\"{'gantt-bar-overlapped':isOverlapped}\">\n" +
+    "  <div ng-transclude></div>\n" +
+    "</div>");
 }]);
 
 angular.module("views/gantt_bar_container.html", []).run(["$templateCache", function($templateCache) {
@@ -21,30 +21,30 @@ angular.module("views/gantt_bar_container.html", []).run(["$templateCache", func
 angular.module("views/gantt_chart.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("views/gantt_chart.html",
     "<div class=\"gantt-container\">\n" +
-      "  <div class=\"gantt-left-container\">\n" +
-      "    <div class=\"gantt-label-title\" ng-style=\"{top:scrollTop + 'px'}\">\n" +
-      "      {{ ngLabelTitle }}\n" +
-      "    </div>\n" +
-      "    <div class=\"gantt-labels\"></div>\n" +
-      "  </div>\n" +
-      "\n" +
-      "  <div class=\"gantt-body-container\">\n" +
-      "    <div class=\"gantt-scale-container\" ng-style=\"{top:scrollTop + 'px'}\">\n" +
-      "      <div ng-repeat=\"tic in tics\" class=\"tic\" ng-style=\"{width:ngInterval*unitLength + 'px'}\">\n" +
-      "        <div class=\"tic-label\">{{tic.label}}</div>\n" +
-      "      </div>\n" +
-      "    </div>\n" +
-      "    <div class=\"gantt-row-container\" ng-transclude></div>\n" +
-      "  </div>\n" +
-      "\n" +
-      "  <div class=\"gantt-right-container\">\n" +
-      "    <div class=\"gantt-action-title\" ng-style=\"{top:scrollTop + 'px'}\">\n" +
-      "      {{ ngActionTitle }}\n" +
-      "    </div>\n" +
-      "    <div class=\"gantt-actions\"></div>\n" +
-      "  </div>\n" +
-      "</div>\n" +
-      "");
+    "  <div class=\"gantt-left-container\">\n" +
+    "    <div class=\"gantt-label-title\" ng-style=\"{top:scrollTop + 'px'}\">\n" +
+    "      {{ ngLabelTitle }}\n" +
+    "    </div>\n" +
+    "    <div class=\"gantt-labels\"></div>\n" +
+    "  </div>\n" +
+    "\n" +
+    "  <div class=\"gantt-body-container\">\n" +
+    "    <div class=\"gantt-scale-container\" ng-style=\"{top:scrollTop + 'px'}\">\n" +
+    "      <div ng-repeat=\"tic in tics\" class=\"tic\" ng-style=\"{width:ngInterval*unitLength + 'px'}\">\n" +
+    "        <div class=\"tic-label\">{{tic.label}}</div>\n" +
+    "      </div>\n" +
+    "    </div>\n" +
+    "    <div class=\"gantt-row-container\" ng-transclude></div>\n" +
+    "  </div>\n" +
+    "\n" +
+    "  <div class=\"gantt-right-container\">\n" +
+    "    <div class=\"gantt-action-title\" ng-style=\"{top:scrollTop + 'px'}\">\n" +
+    "      {{ ngActionTitle }}\n" +
+    "    </div>\n" +
+    "    <div class=\"gantt-actions\"></div>\n" +
+    "  </div>\n" +
+    "</div>\n" +
+    "");
 }]);
 
 angular.module("views/gantt_chart_marker.html", []).run(["$templateCache", function($templateCache) {
@@ -70,47 +70,6 @@ angular.module("views/gantt_resizable_bar.html", []).run(["$templateCache", func
 angular.module("views/gantt_row.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("views/gantt_row.html",
     "<div ng-transclude style=\"position: relative\"></div>");
-}]);
-
-angular.module("views/main.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("views/main.html",
-    "<div class=\"header\">\n" +
-      "  <ul class=\"nav nav-pills pull-right\">\n" +
-      "    <li class=\"active\"><a ng-href=\"#\">Home</a></li>\n" +
-      "    <li><a ng-href=\"#\">About</a></li>\n" +
-      "    <li><a ng-href=\"#\">Contact</a></li>\n" +
-      "  </ul>\n" +
-      "  <h3 class=\"text-muted\">angular gantt</h3>\n" +
-      "</div>\n" +
-      "\n" +
-      "<div class=\"jumbotron\">\n" +
-      "  <h1>'Allo, 'Allo!</h1>\n" +
-      "  <p class=\"lead\">\n" +
-      "    <img src=\"images/yeoman.png\" alt=\"I'm Yeoman\"><br>\n" +
-      "    Always a pleasure scaffolding your apps.\n" +
-      "  </p>\n" +
-      "  <p><a class=\"btn btn-lg btn-success\" ng-href=\"#\">Splendid!<span class=\"glyphicon glyphicon-ok\"></span></a></p>\n" +
-      "</div>\n" +
-      "\n" +
-      "<div class=\"row marketing\">\n" +
-      "  <h4>HTML5 Boilerplate</h4>\n" +
-      "  <p>\n" +
-      "    HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites.\n" +
-      "  </p>\n" +
-      "\n" +
-      "  <h4>Angular</h4>\n" +
-      "  <p>\n" +
-      "    AngularJS is a toolset for building the framework most suited to your application development.\n" +
-      "  </p>\n" +
-      "\n" +
-      "  <h4>Karma</h4>\n" +
-      "  <p>Spectacular Test Runner for JavaScript.</p>\n" +
-      "</div>\n" +
-      "\n" +
-      "<div class=\"footer\">\n" +
-      "  <p><span class=\"glyphicon glyphicon-heart\"></span> from the Yeoman team</p>\n" +
-      "</div>\n" +
-      "");
 }]);
 
 'use strict';
@@ -166,30 +125,30 @@ angular.module('angularGanttChart')
       },
       link: function(scope, element, attrs) {
 
-        scope.scrollTop = 0;
+        var debounce = function(func, wait, immediate) {
+          var args, result, thisArg, timeoutId;
 
-        element[0].onscroll = _.debounce(function(e) {
-          scope.scrollTop = e.target.scrollTop;
-          scope.$apply();
-        }, 0);
+          function delayed() {
+            timeoutId = null;
+            if (!immediate) {
+              result = func.apply(thisArg, args);
+            }
+          }
 
-        scope.$watch('ngScrollLeft', function() {
+          return function() {
+            var isImmediate = immediate && !timeoutId;
+            args = arguments;
+            thisArg = this;
 
-//          if (_.isNumber(scope.ngScrollLeft)) {
-//            var scrollLeft = ganttChart.getOffset(scope.ngScrollLeft);
-//            var ganttRowsEle = element.find('.gantt-rows')[0];
-//            var currentScrollLeft = ganttRowsEle.scrollLeft;
-//
-//            var width = $(ganttRowsEle).width();
-//
-//            if (scrollLeft <= currentScrollLeft) {
-//              ganttRowsEle.scrollLeft = scrollLeft;
-//            }
-//            if (scrollLeft >= currentScrollLeft + width) {
-//              ganttRowsEle.scrollLeft = scrollLeft - width;
-//            }
-//          }
-        });
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(delayed, wait);
+
+            if (isImmediate) {
+              result = func.apply(thisArg, args);
+            }
+            return result;
+          };
+        };
 
         var formatTic = function(tic) {
           var t = tic;
@@ -202,13 +161,16 @@ angular.module('angularGanttChart')
         };
 
         var setTics = function() {
-          scope.tics = _.map(_.range(scope.ngBegin, scope.ngEnd, scope.ngInterval), function(tic) {
+          var range = [];
+
+          for (var s = parseFloat(scope.ngBegin); s < parseFloat(scope.ngEnd); s = s + parseFloat(scope.ngInterval)) {
+            range.push(s);
+          }
+          scope.tics = range.map(function(tic) {
             return {label: formatTic(tic)};
           });
           scope.unitLength = element[0].querySelector('.gantt-scale-container').clientWidth / (scope.ngEnd - scope.ngBegin);
         };
-
-        setTics();
 
         scope.render = function() {
           var rows = element[0].querySelectorAll('[gantt-row] > div');
@@ -217,13 +179,27 @@ angular.module('angularGanttChart')
 
           angular.forEach(rows, function(r) {
             var id = angular.element(r).scope().$id;
-            var row = _.where(scope.rows, {id: id})[0];
+            var row;
+            for (var i = 0; i < scope.rows.length; i++) {
+              if (scope.rows[i] && scope.rows[i].id == id) {
+                row = scope.rows[i];
+              }
+            }
             labels.appendChild(row.labelEle);
             actions.appendChild(row.actionEle);
           });
         };
 
-        scope.debRender = _.debounce(scope.render, 100, true);
+        setTics();
+
+        scope.debRender = debounce(scope.render, 100, true);
+
+        scope.scrollTop = 0;
+
+        element[0].onscroll = debounce(function(e) {
+          scope.scrollTop = e.target.scrollTop;
+          scope.$apply();
+        }, 0);
 
         scope.$on('$destroy', function() {
           element[0].onscroll = null;
@@ -253,11 +229,10 @@ angular.module('angularGanttChart')
         }
       },
       link: function postLink(scope, element, attrs, ganttRow) {
-        var debRowRenderer = _.debounce(ganttRow.render, 0);
 
         scope.render = function() {
 
-          _.extend(scope, ganttRow.getScale());
+          angular.extend(scope, ganttRow.getScale());
 
           var offset = 0;
           var slotsMap = [];
@@ -269,8 +244,8 @@ angular.module('angularGanttChart')
 
           var getAvailableSlot = function(startPoint, endPoint) {
 
-            return _.min(_.reduce(slotsMap, function(m, bar, i) {
-              var overlapped = _.any(slotsMap[i], function(bar) {
+            return Math.min(slotsMap.reduce(function(m, bar, i) {
+              var overlapped = slotsMap[i].some(function(bar) {
                 return doesOverlap(startPoint, endPoint, bar.start, bar.end);
               });
               if (!overlapped) {
@@ -299,14 +274,14 @@ angular.module('angularGanttChart')
           };
 
           var isOverlapped = function(bar) {
-            return _.any(scope.bars, function(b) {
+            return scope.bars.some(function(b) {
               return bar.scope.$id == b.scope.$id ? false : doesOverlap(b.scope.ngBegin, b.scope.ngEnd, bar.scope.ngBegin, bar.scope.ngEnd);
             });
           };
 
           scope.height = 0;
 
-          _.each(scope.bars, function(b) {
+          scope.bars.forEach(function(b) {
             b.scope.topOffset = getTopOffset(b);
             var h = b.scope.topOffset + b.element[0].clientHeight;
             if (scope.height < h) {
@@ -320,7 +295,7 @@ angular.module('angularGanttChart')
             b.scope.isOverlapped = isOverlapped(b);
           });
 
-          debRowRenderer();
+          ganttRow.render();
 
         };
       }
@@ -495,7 +470,7 @@ angular.module('angularGanttChart')
             ganttBar.move(offset, offset);
             scope.$apply();
           };
-          if (_.isUndefined(attrs.ngDragEnd)) {
+          if (attrs.ngDragEnd === undefined) {
             moveBar();
           } else if (scope.ngDragEnd()) {
             moveBar();
@@ -553,7 +528,7 @@ angular.module('angularGanttChart')
             scope.ngLeftOnly ? ganttBar.move(offset, 0) : ganttBar.move(0, offset);
             scope.$apply();
           };
-          if (_.isUndefined(scope.ngDragEnd)) {
+          if (scope.ngDragEnd === undefined) {
             moveBar();
           } else if (scope.ngDragEnd()) {
             moveBar();
@@ -589,7 +564,7 @@ angular.module('angularGanttChart')
         scope.$watch('ngBegin + ngEnd', function() {
           scope.style = {};
           scope.style.left = ganttChart.getOffset(scope.ngBegin);
-          if (!_.isUndefined(scope.ngEnd)) {
+          if (scope.ngEnd !== undefined) {
             scope.style.width = ganttChart.getOffset(scope.ngEnd) - scope.style.left;
           }
         });
